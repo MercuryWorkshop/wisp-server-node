94e00d4b255e633a22edf6e95d033c9bc1531d12
ProgrammerIn-wonderland <3838shah@gmail.com>
2024-02-08 17:27:24 -0500

    seperation to better organize

--- src/createServer.ts
+++ src/createServer.ts
@@ -1,18 +1,5 @@
-/** Wisp server node!
- * Made by Rafflesia@\MercuryWorkshop
- * 
- * This software is licensed under AGPLv3 &&
- * You should have a copy of AGPLv3 with this source code
- * 
- * This implimentation of wisp is sort of really broken 
- * so I set the buffer size to max 32bit int, 
- * I'm not sure why it doesn't work with lower values
- * 
- */
-
-
 import { WebSocketServer } from 'ws';
-import wisp from './wisp';
+import wisp from './ConnectionHandler';
 
 const wss = new WebSocketServer({
   port: 3000
