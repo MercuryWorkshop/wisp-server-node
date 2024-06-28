217feca6a59039b783e7a365b9544e218f38e076
builtbyvys <hey@vyslix.dev>
2024-06-28 13:16:29 -0400

    feat: update examples with new wisp protocol

--- examples/basic.js
+++ examples/basic.js
@@ -5,13 +5,19 @@ const wisp = require("wisp-server-node");
 const httpServer = http.createServer();
 
 httpServer.on("upgrade", (req, socket, head) => {
-    wisp.routeRequest(req, socket, head);
+  wisp.routeRequest(req, socket, head, {
+    logging: true,
+    auth: false,
+    throttle: true,
+    throttleLimit: 5 * 1024 * 1024, // throttle limit: 5 MB
+    throttleInterval: 5000, // throttle check interval: 5 seconds
+  });
 });
 
 httpServer.on("listening", () => {
-    console.log("HTTP server listening");
+  console.log("HTTP server listening");
 });
 
 httpServer.listen({
-    port: 8080,
+  port: 8080,
 });
