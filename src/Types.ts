76586922ecf96ec677465db824785c7bdd8bf6b0
builtbyvys <hey@vyslix.dev>
2024-06-28 13:20:29 -0400

    feat: add new types for wisp

--- src/Types.ts
+++ src/Types.ts
@@ -1,18 +1,33 @@
+// src/Types.ts
+
 export type WispFrame = {
-    type: CONNECT_TYPE;
-    streamID: number;
-    payload: Uint8Array;
+  type: CONNECT_TYPE;
+  streamID: number;
+  payload: Uint8Array;
 };
+
 export enum CONNECT_TYPE {
-    CONNECT = 0x01,
-    DATA = 0x02,
-    CONTINUE = 0x03,
-    CLOSE = 0x04,
+  CONNECT = 0x01,
+  DATA = 0x02,
+  CONTINUE = 0x03,
+  CLOSE = 0x04,
+  INFO = 0x05,
 }
+
 export enum STREAM_TYPE {
-    TCP = 0x01,
-    UDP = 0x02,
+  TCP = 0x01,
+  UDP = 0x02,
 }
+
 export type WispOptions = {
-    logging?: boolean;
+  logging?: boolean;
+  auth?: boolean;
+  throttle?: boolean;
+  throttleLimit?: number;
+  throttleInterval?: number;
+};
+
+export type ExtensionInfo = {
+  id: number;
+  payload: Uint8Array;
 };
